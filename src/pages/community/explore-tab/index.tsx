c6c263d7fb85ca336ae77fe12c585c6655709c84
ec50n9 <shelloworld@qq.com>
2023-07-09 10:33:00 +0800

    拆分tab页面

---
+++ src/pages/community/explore-tab/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "@tarojs/components";
+
+function Index() {
+  return <View>这里是社区首页</View>;
+}
+
+export default Index;
---
+++ src/pages/community/square-tab/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "@tarojs/components";
+
+function Index() {
+  return <View>这里是广场</View>;
+}
+
+export default Index;
