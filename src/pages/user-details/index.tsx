0ee666b9ecee82fe85cf1169efa2a3da785fb9a1
ec50n9 <shelloworld@qq.com>
2023-09-07 22:54:26 +0800

    个人详情页面改为unocss实现

--- src/pages/user-details/index.tsx
+++ src/pages/user-details/index.tsx
@@ -1,4 +1,3 @@
-import { Flex } from "@taroify/core";
 import {
   ArrowLeft,
   CartOutlined,
@@ -7,12 +6,11 @@ import {
   Star,
   StarOutlined,
 } from "@taroify/icons";
-import { View, Image } from "@tarojs/components";
+import { Image } from "@taroify/core";
+import { View } from "@tarojs/components";
 import { getCurrentInstance } from "@tarojs/runtime";
 import { usePageScroll } from "@tarojs/taro";
 import { useEffect, useRef, useState } from "react";
-import { Article } from "/src/pages/community/api";
-import "./index.scss";
 
 type UserDetails = {
   id: string;
@@ -36,17 +34,25 @@ type Article = {
 function ArticleItem(props: { article: Article }) {
   const article = props.article;
   return (
-    <View className='article-item'>
-      <View className='article-item__cover' />
-      <View className='article-item__title'>{article.title}</View>
-      <View className='article-item__info'>
-        <View className='article-item__info__avatar'>
-          <Image src={article.author.avatar} />
+    <View className='flex flex-col gap-2 h-500 c-gray-7 bg-white'>
+      <View className='grow object-cover rd-3 of-hidden bg-blue-3'>
+        <Image
+          className='w-full h-full'
+          mode='aspectFill'
+          src={article.author.avatar}
+        />
+      </View>
+      <View className='shrink-0 of-hidden truncate'>{article.title}</View>
+      <View className='shrink-0 flex items-center gap-1 c-gray-5'>
+        <View className='shrink-0 w-40 h-40 bg-gray-5 rd-full of-hidden'>
+          <Image
+            className='w-full h-full'
+            mode='aspectFill'
+            src={article.author.avatar}
+          />
         </View>
-        <View className='article-item__info__author'>
-          {article.author.nickname}
-        </View>
-        <View className='article-item__info__like'>
+        <View className='grow'>{article.author.nickname}</View>
+        <View className='shrink-0'>
           <LikeOutlined style={{ marginRight: "4px" }} />
           {article.likeCount}
         </View>
@@ -57,7 +63,7 @@ function ArticleItem(props: { article: Article }) {
 
 function ArticleList(props: { articles: Article[] }) {
   return (
-    <View className='article-list'>
+    <View className='grid grid-cols-2 gap-5 px-5 pb-5 bg-white'>
       {props.articles.map((article) => (
         <ArticleItem article={article} key={article.id} />
       ))}
@@ -77,7 +83,7 @@ export default function Index() {
       username: "abc",
       nickname: "陈霸先",
       signture: "每个人都有快乐的时候，你呢？",
-      avatar: "abc",
+      avatar: "https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80",
       following: 97,
       followers: 634,
       isFollowed: false,
@@ -86,11 +92,12 @@ export default function Index() {
       {
         id: "1",
         title: "文章1",
-        cover: "",
+        cover: "https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80",
         likeCount: 100,
         author: {
+          id: "abc",
           nickname: "作者1",
-          avatar: "",
+          avatar: "https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1024&h=1280&q=80",
         },
       },
       {
@@ -146,69 +153,71 @@ export default function Index() {
     ]);
   }, []);
 
-  const pageSize = 20;
   const [articleList, setarticleList] = useState<Article[]>([]);
-  const [hasMore, setHasMore] = useState(true);
-  const [loading, setLoading] = useState(false);
-  const refreshingRef = useRef(false);
-  const [scrollTop, setScrollTop] = useState(0);
-  const [reachTop, setReachTop] = useState(true);
-  const [pageNo, setPageNo] = useState<number>(1);
+  // const pageSize = 20;
+  // const [hasMore, setHasMore] = useState(true);
+  // const [loading, setLoading] = useState(false);
+  // const refreshingRef = useRef(false);
+  // const [scrollTop, setScrollTop] = useState(0);
+  // const [reachTop, setReachTop] = useState(true);
+  // const [pageNo, setPageNo] = useState<number>(1);
 
-  usePageScroll(({ scrollTop: aScrollTop }) => {
-    setScrollTop(aScrollTop);
-    setReachTop(aScrollTop === 0);
-  });
+  // usePageScroll(({ scrollTop: aScrollTop }) => {
+  //   setScrollTop(aScrollTop);
+  //   setReachTop(aScrollTop === 0);
+  // });
 
-  function onLoad(pageNo: number) {
-    console.log(`推荐内容-on load, pageNo:${pageNo}`);
-  }
+  // function onLoad() {
+  //   console.log(`推荐内容-on load, pageNo:${pageNo}`);
+  // }
 
-  function onRefresh() {
-    console.log("推荐内容-on refresh");
-    refreshingRef.current = true;
-    setLoading(false);
-    onLoad(pageNo);
-  }
+  // function onRefresh() {
+  //   console.log("推荐内容-on refresh");
+  //   refreshingRef.current = true;
+  //   setLoading(false);
+  //   onLoad();
+  // }
 
   return (
-    <View className='user-details'>
-      <View className='header'>
+    <View className='min-h-screen c-white text-base bg-gray-7'>
+      <View className='p-3'>
         <ArrowLeft />
       </View>
 
-      <View className='user-info'>
-        <View className='user-info__avatar'></View>
-        <View className='user-info__center'>
-          <View className='user-info__name'>{userDetails?.nickname}FV</View>
-          <View className='user-info__id'>账户ID：{userDetails?.id}</View>
-          <View className='user-info__signture'>
+      <View className='flex items-center gap-3 p-3'>
+        <View className='w-96 h-96 rd-full of-hidden bg-blue-200'>
+        <Image
+          className='w-full h-full'
+          mode='aspectFill'
+          src={userDetails?.avatar}
+        />
+        </View>
+        <View className='grow'>
+          <View className='text-base'>{userDetails?.nickname}FV</View>
+          <View className='text-sm c-gray-3'>账户ID：{userDetails?.id}</View>
+          <View className='text-xs c-gray-3'>
             {userDetails?.signture ?? "这个人很懒，什么都没有留下"}
           </View>
         </View>
-        <View className='user-info__follow'>
+        <View className='shrink-0 w-72 h-72 flex center rd-full text-lg bg-white bg-opacity-30'>
           {userDetails?.isFollowed ? <Star /> : <StarOutlined />}
         </View>
-        <View className='user-info__contact'>
+        <View className='shrink-0 w-72 h-72 flex center rd-full text-lg bg-white bg-opacity-30'>
           <CommentOutlined />
         </View>
       </View>
 
-      <View className='user-statistics'>
-        <View className='user-statistics__item user-statistics__item--shop'>
-          <CartOutlined /> ta的小店
+      <View className='flex gap-12 items-center rd-t-8 c-gray-7 bg-white px-5 py-3'>
+        <View className='shrink-0 flex flex-col items-center grow items-center'>
+          <CartOutlined size={24} /> ta的小店
         </View>
-        <View className='user-statistics__item'>
-          <View className='user-statistics__item__value'>
-            {userDetails?.following}
-          </View>
-          <View className='user-statistics__item__label'>关注</View>
+        <View className='shrink-0 flex flex-col items-center'>
+          <View className='font-bold'>{userDetails?.following}</View>
+          <View className='text-sm px-1 bg-gray-7 bg-opacity-30'>关注</View>
         </View>
-        <View className='user-statistics__item'>
-          <View className='user-statistics__item__value'>
-            {userDetails?.followers}
-          </View>
-          <View className='user-statistics__item__label'>粉丝</View>
+        <View className='shrink-0 flex flex-col items-center'>
+          <View className='font-bold'>{userDetails?.followers}</View>
+          <View className='text-sm px-1 bg-gray-7 bg-opacity-30'>粉丝</View>
         </View>
       </View>
 
