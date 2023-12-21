7e7cd9cc5cc6f50bc7fb81310fe7b0311fd477f6
ec50n9 <shelloworld@qq.com>
2023-12-21 23:22:08 +0800

    修正活动进度文本显示

--- src/pages/creator-center/components/Activity.tsx
+++ src/pages/creator-center/components/Activity.tsx
@@ -6,25 +6,22 @@ type ActivityProps = {
 };
 
 const EcProgress = (props: {
-  steps: (number | null)[];
+  steps: { percent: number; label: string }[];
   percent: number;
   label: string;
 }) => {
   return (
     <View className='mt-3 w-full'>
-      <View className='flex justify-between'>
-        {props.steps.map((item, index) =>
-          item ? (
-            <View
-              key={index}
-              className='px-2 text-xs bg-orange-3 c-orange-7 rd-1'
-            >
-              {item}
-            </View>
-          ) : (
-            <View className='w-20'></View>
-          )
-        )}
+      <View className='relative h-30'>
+        {props.steps.map((item, index) => (
+          <View
+            key={index}
+            className='absolute top-1/2 transform -translate-y-1/2 -translate-x-1/2 px-2 text-xs bg-orange-3 c-orange-7 rd-1'
+            style={{ left: `${item.percent * 100}%` }}
+          >
+            {item.label}
+          </View>
+        ))}
       </View>
       <Progress className='mt-1' percent={props.percent} label={props.label} />
     </View>
@@ -39,7 +36,15 @@ export default function (props: ActivityProps) {
       <View className='mt-3 text-lg'>已连续签到 {props.days} 天</View>
       <View className='mt-1 text-sm c-gray-4'>连续签到 7 天享受更高奖励</View>
       <EcProgress
-        steps={[null, 10, 20, 40, 80, 150, 200, 600]}
+        steps={[
+          { percent: 0.1, label: "10" },
+          { percent: 0.2, label: "20" },
+          { percent: 0.3, label: "40" },
+          { percent: 0.4, label: "80" },
+          { percent: 0.6, label: "150" },
+          { percent: 0.8, label: "200" },
+          { percent: 1, label: "600" },
+        ]}
         percent={(props.days / 7) * 100}
         label={`还差 ${7 - props.days} 天`}
       />
